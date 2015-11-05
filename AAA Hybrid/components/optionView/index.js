81b7330f6cace370c0a595416f338b04e774b8d7
Kerem Gökhan <e.keremgokhan@gmail.com>
2015-11-05 02:07:24 +0000

    Fisinish the last page with dummy design dummy data.

--- AAA Hybrid/components/optionView/index.js
+++ AAA Hybrid/components/optionView/index.js
@@ -6,4 +6,13 @@ app.optionView = kendo.observable({
 });
 
 // START_CUSTOM_CODE_optionView
+
+function removeAllActives() {
+    
+}
+
+$("body").on("click", ".touch", function(e) {
+    $(".active").removeClass("active"); 
+    $(this).parent().addClass("active");
+});
 // END_CUSTOM_CODE_optionView
